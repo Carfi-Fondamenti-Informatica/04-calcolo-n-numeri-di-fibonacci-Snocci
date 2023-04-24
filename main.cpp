7fb5a590c2f98eaba53c1e0656f153dabe970bf6
Snocci <128066678+Snocci@users.noreply.github.com>
2023-04-24 15:20:23 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
+   int a = 1, b = 1, z = 3;
+    int n, Fibonacci;
+    cin >> n;
+    if (n >= 2) {
+        cout << "1" << endl;
+        cout << "1" << endl;
+
+        while (z <= n) {
+            Fibonacci = a + b;
+            cout << Fibonacci << endl;
+            b = a;
+            a = Fibonacci;
+            z = z + 1;
+        }
+        cout << endl;
+    } else
+        cout << "Errore";
+
    return 0;
 }
